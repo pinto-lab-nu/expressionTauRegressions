aa2bb31c532b70aa3640c93f6186195e885421fe
Lyn-Ackert-Smith <lyn.ackertsmith@northwestern.edu>
2024-08-09 18:09:25 -0500

    update to job

---
+++ regressionJob.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=48:00:00
+#SBATCH --job-name=tauRegression
+#SBATCH --mem=15GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=/mnt/fsmresfiles/Tau_Processing/H3/SlurmLogs/regression_%j.txt
+#SBATCH --array=0-3%4
+
+#['Rpb4-Ai96','Cux2-Ai96','C57BL6/J']
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate allenSDKenv
+
+python expressionRegression.py $SLURM_ARRAY_TASK_ID 1
