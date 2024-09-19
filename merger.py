416fe5687ed8c30cd83229f3e4508a6e289f087e
Lyn-Ackert-Smith <57812442+Lyn-Ackert-Smith@users.noreply.github.com>
2024-09-19 01:40:50 -0500

    Update merger.py

--- merger.py
+++ merger.py
@@ -21,28 +21,30 @@ for prePath in ['Spatial',lineSelection]:
         for predictionPath in ['H3Predictors','GenePredictors']: #include " '' " in array here, or below?
 
             for datasetPath,layerNames in zip(['Pilot','Merfish','Merfish-Imputed'],[pilotLayerNames,merfishLayerNames,merfishLayerNames]):
-
+                
                 if prePath == lineSelection:
                     currentPath = os.path.join(savePath,prePath,f'pooling{tauPoolSize}mm',predictionPath,datasetPath)
                 if (prePath == 'Spatial') and (poolIDX == 0):
                     currentPath = os.path.join(savePath,prePath,predictionPath,datasetPath)
                 
-                # Regex to capture portions before and after the variable part
-                pattern = re.compile(r'^(.*?)(_L\d+.* [EI]T)(.*)$')
+                if os.path.exists(currentPath):
 
-                unique_file_names = set()
+                    # Regex to capture portions before and after the variable part
+                    pattern = re.compile(r'^(.*?)(_L\d+.* [EI]T)(.*)$')
 
-                for file in os.listdir(currentPath):
-                    if (file[-4:] == '.pdf'):
-                        match = pattern.match(file)
-                        if match:
-                            combined = match.group(1) + '*****' + match.group(3)
-                            unique_file_names.add(combined)
+                    unique_file_names = set()
 
-                unique_file_names = sorted(unique_file_names)
+                    for file in os.listdir(currentPath):
+                        if (file[-4:] == '.pdf'):
+                            match = pattern.match(file)
+                            if match:
+                                combined = match.group(1) + '*****' + match.group(3)
+                                unique_file_names.add(combined)
 
-                for rename in unique_file_names:
-                    split0,split1 = rename.split('*****')
-                    if os.path.exists(os.path.join(currentPath,split0+'_'+layerNames[0]+split1)):
-                        PDFmerger(currentPath,split0+'_',layerNames,split1,split0+split1)
+                    unique_file_names = sorted(unique_file_names)
+
+                    for rename in unique_file_names:
+                        split0,split1 = rename.split('*****')
+                        if os.path.exists(os.path.join(currentPath,split0+'_'+layerNames[0]+split1)):
+                            PDFmerger(currentPath,split0+'_',layerNames,split1,split0+split1)
                 
