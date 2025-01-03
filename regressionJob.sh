862827151e0502f5811d917eea23e7a260ec8bcb
Lyn-Ackert-Smith <57812442+Lyn-Ackert-Smith@users.noreply.github.com>
2025-01-03 14:18:33 -0600

    Update regressionJob.sh

--- regressionJob.sh
+++ regressionJob.sh
@@ -17,26 +17,25 @@ conda activate allenSDKenv
 # Parse command line arguments for all variables
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -g|--geneLimit) geneLimit="$2"; shift ;;
-        -l|--lineSelection) lineSelection="$2"; shift ;;
-        -r|--restrict_merfish_imputed_values) restrict_merfish_imputed_values="$2"; shift ;;
-        -t|--tauPoolSizeArrayFull) tauPoolSizeArrayFull="$2"; shift ;;
-        -n|--n_splits) n_splits="$2"; shift ;;
-        -a|--alphaParams) alphaParams="$2"; shift ;;
-        -d|--loadData) loadData="$2"; shift ;;
-        -p|--plotting) plotting="$2"; shift ;;
-        -u|--numPrecision) numPrecision="$2"; shift ;;
-        -e|--alphaPrecision) alphaPrecision="$2"; shift ;;
-        -v|--verbose) verbose="$2"; shift ;;
-        -o|--predictorOrder) predictorOrder="$2"; shift ;;
-        -s|--regressionsToStart) regressionsToStart="$2"; shift ;;
-        -m|--max_iter) max_iter="$2"; shift ;;
-        -c|--variableManagement) variableManagement="$2"; shift ;;
-        -k|--plottingConditions) plottingConditions="$2"; shift ;;
-        -x|--arg_parse_test) arg_parse_test="$2"; shift ;;
+        -g|--geneLimit) geneLimit="$2"; shift 2 ;;
+        -l|--lineSelection) lineSelection="$2"; shift 2 ;;
+        -r|--restrict_merfish_imputed_values) restrict_merfish_imputed_values="$2"; shift 2 ;;
+        -t|--tauPoolSizeArrayFull) tauPoolSizeArrayFull="$2"; shift 2 ;;
+        -n|--n_splits) n_splits="$2"; shift 2 ;;
+        -a|--alphaParams) alphaParams="$2"; shift 2 ;;
+        -d|--loadData) loadData="$2"; shift 2 ;;
+        -p|--plotting) plotting="$2"; shift 2 ;;
+        -u|--numPrecision) numPrecision="$2"; shift 2 ;;
+        -e|--alphaPrecision) alphaPrecision="$2"; shift 2 ;;
+        -v|--verbose) verbose="$2"; shift 2 ;;
+        -o|--predictorOrder) predictorOrder="$2"; shift 2 ;;
+        -s|--regressionsToStart) regressionsToStart="$2"; shift 2 ;;
+        -m|--max_iter) max_iter="$2"; shift 2 ;;
+        -c|--variableManagement) variableManagement="$2"; shift 2 ;;
+        -k|--plottingConditions) plottingConditions="$2"; shift 2 ;;
+        -x|--arg_parse_test) arg_parse_test="$2"; shift 2 ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
-    shift
 done
 
 # Default values if not provided
